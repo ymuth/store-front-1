a53d46a94c15fb3b50a9a258b51041f3b2ce9fe9
ymuth <135130958+ymuth@users.noreply.github.com>
2026-07-22 02:15:26 +0100

    ""fix

--- src/components/home/booking.tsx
+++ src/components/home/booking.tsx
@@ -63,9 +63,9 @@ export default function BookingSection() {
 
 
 
-                <form onSubmit={submit} className="mx-auto md:w-full max-w-5xl bg-[#191919] p-8 rounded-xl border border-gray-600 shadow-xl">
+                <form onSubmit={submit} className="mx-auto w-full max-w-5xl bg-[#191919] p-8 rounded-xl border border-gray-600 shadow-xl">
 
-                    <div className="grid md:grid-cols-2 gap-6 overflow-x-clip">
+                    <div className="grid md:grid-cols-2 gap-6 max-w-full overflow-x-clip">
 
                         {/* Name */}
                         <div className="flex flex-col">
